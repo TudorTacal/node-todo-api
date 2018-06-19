b8487881a55ed7a329b35a4c3014b6064e81db82
Tudor Tacal <t.tacal@elsevier.com>
2018-06-19 21:09:57 +0100

    Set up Mongo db & add documents to collections

---
+++ README.md
@@ -0,0 +1,1 @@
+NodeJS TODO API
---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Todos').insertOne({
+    test: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert todo', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });  
+
+  db.collection('Users').insertOne({
+    name: 'Tudor',
+    age: 29,
+    location: 'London'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert User');
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  })
+
+  db.close();
+});
+
