6ca578f57497ac94c5d16d1f71dedd3c7a7b71be
Tudor Tacal <t.tacal@elsevier.com>
2018-06-21 22:36:33 +0100

    Create model and use property validators

--- server/server.js
+++ server/server.js
@@ -6,20 +6,45 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 const Todo = mongoose.model('Todo', {
   text: {
     type: String,
+    required: true,
+    minLength: 1,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
   completedAt: {
     type: Number,
+    default: null
   }
 });
 
-const newTodo = new Todo({});
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  }
+})
+const newTodo = new Todo({
+  text: true,
+});
 
 newTodo.save().then((doc) => {
   console.log('Saved todo', doc);
   
 }, (e) => {
-  console.log('Unable to save todo');
+  console.log('Unable to save todo', e);
+});
+
+const newUser = new User({
+  email: 'tudor@email.com'
+})
+
+newUser.save().then(doc => {
+  console.log('Saved user', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
 });
